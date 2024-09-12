7ba596470e6aaafae207130b8742f88edd955b5a
aman <amandeeppos@gmail.com>
2024-09-12 05:37:51 +0530

    test 11

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,9 +6,9 @@ import router from "./router/router.tsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <GoogleOAuthProvider clientId="489195454771-8k3ddoduoaakqlv47nqq21q971t2s02i.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId="489195454771-8k3ddoduoaakqlv47nqq21q971t2s02i.apps.googleusercontent.com">
+    <StrictMode>
       <RouterProvider router={router} />
-    </GoogleOAuthProvider>
-  </StrictMode>
+    </StrictMode>
+  </GoogleOAuthProvider>
 );
